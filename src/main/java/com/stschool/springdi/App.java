95659c6f9567c5b5f59085b76a5f5f85252fc33b
arunkumar-7 <arunkumarbathini4@gmail.com>
2026-06-02 16:38:39 +0530

    feat: Add Spring dependency injection between Product and order

--- src/main/java/com/stschool/springdi/App.java
+++ src/main/java/com/stschool/springdi/App.java
@@ -8,8 +8,12 @@ public class App
 {
     public static void main( String[] args ){
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        Product product = context.getBean("1",Product.class);
-        System.out.println(product);
+//        Product product1 = context.getBean("1",Product.class);
+      /*  System.out.println(product1);
+        Product product2 = context.getBean("2",Product.class);
+        System.out.println(product2);*/
+        Order order1 = context.getBean("order1",Order.class);
+        System.out.println(order1);
     }
 
 }
--- src/main/java/com/stschool/springdi/Order.java
+++ src/main/java/com/stschool/springdi/Order.java
@@ -8,10 +8,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 
+public class Order {
+    private int orderId;
+    private int quantity;
+    private Product product;
 
-public class Product {
-    private int id;
-    private String name;
-    private int price;
-    private int discount;
 }
